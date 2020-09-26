de2f4a0c3273764dc2a6154c0f0522f2bf2c6856
darubiano57 <darubiano57@ucatolica.edu.co>
2020-09-26 16:42:44 -0500

    fase 1

---
+++ index.js
@@ -0,0 +1,29 @@
+/*
+npm init -y
+npm i express
+npm i dotenv
+npm i socket.io
+# probar
+node index.js
+# modo demonio
+npm i -g nodemon
+nodemon index.js
+*/
+// importacion
+const express = require('express');
+const path = require('path');
+const app = express();
+// Configuracion de variables de entorno
+require('dotenv').config();
+// Node server
+const server = require('http').createServer(app);
+module.exports.io = require('socket.io')(server);
+require('./sockets/socket')
+//Path publico
+const publicPath = path.resolve(__dirname, 'public');
+app.use(express.static(publicPath));
+
+server.listen(process.env.PORT, (err)=>{
+    if (err) throw new Error(err);
+    console.log('Servidor corriendo en puerto',process.env.PORT);
+});
